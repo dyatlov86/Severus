b44207a5eb8366e14001f819749936742974b6a7
dyatlov86 <onurozturk97@hotmail.com>
2023-11-05 20:04:36 +0300

    agent pid and get the job

--- agent/agent.py
+++ agent/agent.py
@@ -1,9 +1,20 @@
 import frida
-import sys,time,psutil,os
+import sys,psutil,os,configparser
 from multiprocessing import Process
+import http.server,socketserver,requests
 func=[]
 
-import http.server,socketserver
+def get_the_task():
+    req=requests.get('http://192.168.56.1:8080/task.ini')
+    config=configparser.ConfigParser(allow_no_value=True)
+    config.read_string(req.text)
+    must_be_returned=[config["TASK"][var] for var in config["TASK"]]
+    return must_be_returned
+
+def get_pid(name):
+    proc = filter(lambda p: p.name() == name, psutil.process_iter())
+    for i in proc:
+        return i.pid
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -16,21 +27,18 @@ def server():
 def on_message(message, data):
     global func
     if message['type'] == 'send':
-#        print(message['payload'])
         if not message['payload'] in func:
             f=open(os.getcwd()+"\\output\\exports.txt","a+")
             f.write(message["payload"]+"\n")
             f.close()
+            print(message['payload'])
             func.append(message['payload'])
-    elif message['type'] == 'error':
-        print(message['stack'])
-    else:
-     print(message)
+
 
 
 def run():
     
-    pid=frida.spawn(sys.argv[1])
+    pid=get_pid(sys.argv[1])
     session = frida.attach(pid)
 
     f=open(os.getcwd()+"\\dll_exports\\user32.dll.txt","r")
@@ -40,17 +48,15 @@ def run():
     js+=f.read()
     f.close()
     script = session.create_script(js)
-
-
     script.on('message', on_message)
     script.load()
-    frida.resume(pid)
     sys.stdin.read()
 
 
 
 
 if __name__ == "__main__":
+    print(get_the_task())
     process1 = Process(target=server)
     process1.start()
     run()
